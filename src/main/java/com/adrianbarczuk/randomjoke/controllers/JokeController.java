ae47575695900a61c7bb5ae12afb45f008f05739
adrianb92 <adrian.barczuk@gmail.com>
2018-07-18 19:40:12 +0200

    Add Joke Controller

---
+++ src/main/java/com/adrianbarczuk/randomjoke/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.adrianbarczuk.randomjoke.controllers;
+
+import com.adrianbarczuk.randomjoke.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping(value={"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "joke";
+
+    }
+}

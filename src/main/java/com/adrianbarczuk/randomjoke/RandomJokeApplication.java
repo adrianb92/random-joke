88b3a510a0a94769b78f35066d89b19a839d458b
adrianb92 <adrian.barczuk@gmail.com>
2018-07-18 19:14:40 +0200

    First commit

---
+++ src/main/java/com/adrianbarczuk/randomjoke/RandomJokeApplication.java
@@ -0,0 +1,12 @@
+package com.adrianbarczuk.randomjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomJokeApplication.class, args);
+	}
+}
